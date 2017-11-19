3c925b27fd1d5770c1b6edcea1c4a4d3d1df7c06
ozgursahin <ozgursahin@zoho.com>
2017-11-20 01:11:15 +0300

    Angular.io tutorial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // NgModel is in here
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroesComponent,
+		HeroDetailComponent,
+		MessagesComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [HeroService, MessageService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
